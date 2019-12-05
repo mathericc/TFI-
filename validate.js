b9fb39e4b9e9d85b412ddc63a1754f4ba98fbdab
Hercules-Herculianus <46635293+Hercules-Herculianus@users.noreply.github.com>
2019-12-04 22:33:53 -0300

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,1 +1,29 @@
+function validate(){
+	var nome = document.getElementById('nome').value
+	var email = document.getElementById('email').value
+	var senha = document.getElementById('senha1').value
+	var senha2 = document.getElementById('senha2').value
 
+	if (nome == ""){
+		alert('Preencha o campo nome!');
+	}
+
+	else if (email == "" || email.indexOf('@') === -1 ||  email.indexOf(".") === -1 || email.length < 5){
+		alert("Preencha o campo do email corretamente!");
+	}
+
+	else if (senha != senha2 ) {
+		alert("As senhas não coincidem!");
+
+	}
+	else if (senha == ""){
+		alert("Preencha o campo da senha!");
+	}
+	else if (senha2 == ""){
+		alert("Preencha o campo da confirmação de senha!");
+	}
+	else {
+		console.log("aaaaaa");
+		return true;
+	}
+}
