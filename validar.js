edc9c4ea2434c4986257f22b3d7fa39167552adc
Hercules-Herculianus <46635293+Hercules-Herculianus@users.noreply.github.com>
2019-12-04 23:26:01 -0300

    Update validar.js

--- validar.js
+++ validar.js
@@ -1,1 +1,14 @@
 
+function validar(){
+	var email = document.getElementById('email').value
+	var senha = document.getElementById('senha1').value
+
+	if (senha == ""){
+		alert("Preencha o campo 'senha'!")
+	}
+	else if (email == "" || email.indexOf('@') === -1 ||  email.indexOf(".") === -1 || email.length < 5){
+		alert("Preencha o campo com um email válido!");
+	}else{
+		return true;
+	}
+}
